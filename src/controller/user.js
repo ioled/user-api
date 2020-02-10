68b71ace2bdce2c3fa1100e6d8ae098eb59a4856
Diego Sepúlveda <diegosepu.soto@gmail.com>
2020-02-10 15:17:21 -0300

    Minor errors fixed

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,29 +1,32 @@
 var _ = require('lodash');
-const mongoose = require('mongoose');
 
-const User = require('../services/mongoDB');
+const {users} = require('../services/mongoDB');
 
 /**
  * Returns the current authenticated user.
  * @param {{user: object}} req Request. Passport.js sets req.user object.
  * @param {object} res Respose.
  */
-
 exports.currentUser = async (req, res) => {
   console.log('[User-API][currentUser][Request]', req.params, req.body);
 
   // If user is not authenticated, return null.
   if (!req.body) {
     console.log('[User-API][currentUser][Error]', {error: 'User not logged in'});
-    res.json({error: 'User not logged in'});
+    res.status(500).json({error: 'User not logged in'});
   } else {
-    googleID = req.body.googleID;
+    const {googleID} = req.body;
 
-    // Search in the DB for the user.
-    const existingUser = await User.users.findOne({googleID});
-    const userInfo = _.pick(existingUser, ['name', 'email', 'photo']);
+    try {
+      // Search in the DB for the user.
+      const existingUser = await users.findOne({googleID});
+      const userInfo = _.pick(existingUser, ['name', 'email', 'photo']);
 
-    console.log('[User-API][currentUser][Response]', userInfo);
-    res.status(200).send(userInfo);
+      console.log('[User-API][currentUser][Response]', userInfo);
+      res.status(200).send(userInfo);
+    } catch (error) {
+      console.log('[User-API][currentUser][Error]', error);
+      res.status(500).json(error);
+    }
   }
 };
